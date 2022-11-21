10914f333c64720cf0cdf7f212ced256e124a870
Saulo Alves Magalhães <saulo.magalhaes@outlook.com.br>
2022-11-21 20:50:14 -0300

    feat: desafio media da turma concluido

---
+++ Semana 2 - Explorando a Linguagem/Média da Turma/Aluno.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Média_da_Turma
+{
+    public class Aluno
+    {
+        public double Nota { get; private set; }
+        
+        public void AdicionaNota(double nota)
+        {
+            Nota = nota;
+        }
+    }
+}
---
+++ Semana 2 - Explorando a Linguagem/Média da Turma/Program.cs
@@ -0,0 +1,31 @@
+﻿using Média_da_Turma;
+
+Aluno Ana = new Aluno();
+Ana.AdicionaNota(6.8);
+
+Aluno Bia = new Aluno();
+Bia.AdicionaNota(7.9);
+
+Aluno Caio= new Aluno();
+Caio.AdicionaNota(6.1);
+
+Aluno Dani = new Aluno();
+Dani.AdicionaNota(10);
+
+Aluno Eli  = new Aluno();
+Eli.AdicionaNota(5.4);
+
+List<Aluno> notasDosAlunos = new List<Aluno>
+{
+    Ana,
+    Bia,
+    Caio,
+    Dani,
+    Eli
+};
+
+double mediaDaTurma = Turma.MediaDaTurma(notasDosAlunos);
+string aprovacao = "Turma reprovada!";
+if(mediaDaTurma > 7) aprovacao= "Turma aprovada!";
+
+Console.WriteLine($"A média da turma foi de {mediaDaTurma} - {aprovacao}");

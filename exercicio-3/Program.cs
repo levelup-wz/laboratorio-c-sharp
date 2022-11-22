fc992af3198462173d3bd7d023f2399e3376878c
Davi Kennedy Alves Rodrigues <davi_kennedy@live.com>
2022-11-21 22:44:42 -0300

    Adicionando resolução do exercício 'Imprimir a soma dos números de 1 a 100' :white_check_mark

---
+++ exercicio-3/Program.cs
@@ -0,0 +1,21 @@
+﻿// Imprimir a soma dos números de 1 a 100
+
+
+// Resolução com 'for'
+int soma = 0;
+
+Console.WriteLine();
+for (int i = 0; i <= 100; i++) {
+    soma += i;
+}
+Console.WriteLine($"Usando o laço 'for': {soma}");
+
+// Resolução com 'while'
+soma = 0;
+int i = 0;
+
+while (i <= 100) {
+    soma += i;
+    i++;
+}
+Console.WriteLine($"Usando o laço 'while': {soma}");

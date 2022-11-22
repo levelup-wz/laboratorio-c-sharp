309d0b61ca56d48942873dc1bb44ab753cac4dba
Saulo Alves Magalhães <saulo.magalhaes@outlook.com.br>
2022-11-22 10:33:11 -0300

    feat: cria funcao que retorna numeros impares de 0 a 100

---
+++ Semana 2 - Explorando a Linguagem/Numeros Impares de 1 a 100/Program.cs
@@ -0,0 +1,13 @@
+﻿int numero = 1;
+while (numero <= 100)
+{
+    if(numero % 2 == 1)
+    {
+       Console.WriteLine(numero);
+       numero += 1;
+    }
+    else
+    {
+        numero += 1;
+    }
+}

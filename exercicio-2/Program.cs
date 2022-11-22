820b0e295adf2a53be8e976e136b426562e762a4
Davi Kennedy Alves Rodrigues <davi_kennedy@live.com>
2022-11-21 22:19:14 -0300

    Adicionando resolução do exercício 'Imprimir os números ÍMPARES de 1 a 100' :white_check_mark:

---
+++ exercicio-2/Program.cs
@@ -0,0 +1,19 @@
+﻿// Imprimir os números ÍMPARES de 1 a 100
+
+// Resolução com 'for'
+Console.WriteLine("Usando o laço 'for':");
+for (int i = 1; i <= 100; i++) {
+    if (i % 2 != 0) {
+        Console.WriteLine(i);
+    }
+}
+
+// Resolução com 'while'
+Console.WriteLine("\nUsando o laço 'while':");
+int i = 1;
+while (i < 100) {
+    if (i % 2 != 0) {
+        Console.WriteLine(i);
+    }
+    i++;
+}

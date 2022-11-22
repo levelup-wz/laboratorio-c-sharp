d96a2e0fce04ebdf5b49cb89a00de4fd2285a263
Saulo Alves Magalhães <saulo.magalhaes@outlook.com.br>
2022-11-22 12:22:47 -0300

    feat: cria funcao que retorna soma dos numeros de 1 a 100

---
+++ Semana 2 - Explorando a Linguagem/Soma dos numeros de 1 a 100/Program.cs
@@ -0,0 +1,19 @@
+﻿Console.WriteLine("COM WHILE");
+int numero = 1;
+int soma = 0;
+while(numero <= 100)
+{
+    soma += numero;
+    numero+=1;
+}
+Console.WriteLine(soma);
+Console.WriteLine("---------------------------------------------------------");
+
+Console.WriteLine("COM FOR");
+int soma2 = 0;
+for(int i = 1; i <= 100; i++)
+{
+    soma2 += i;
+}
+Console.WriteLine(soma2);
+Console.WriteLine("---------------------------------------------------------");

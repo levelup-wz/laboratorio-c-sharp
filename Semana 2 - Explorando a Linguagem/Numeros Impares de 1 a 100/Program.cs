071c15e3b9fdd71c855285be9f6e96a57dbd856c
Saulo Alves Magalhães <saulo.magalhaes@outlook.com.br>
2022-11-22 12:13:22 -0300

    feat: cria solucao utilizando for

--- Semana 2 - Explorando a Linguagem/Numeros Impares de 1 a 100/Program.cs
+++ Semana 2 - Explorando a Linguagem/Numeros Impares de 1 a 100/Program.cs
@@ -1,4 +1,10 @@
-﻿int numero = 1;
+﻿using System;
+
+Console.WriteLine("---------------------------------------------------------");
+Console.WriteLine("COM WHILE");
+Console.WriteLine("---------------------------------------------------------");
+
+int numero = 1;
 while (numero <= 100)
 {
     if(numero % 2 == 1)
@@ -9,5 +15,18 @@ while (numero <= 100)
     else
     {
         numero += 1;
+    }
+}
+
+Console.WriteLine("---------------------------------------------------------");
+Console.WriteLine("COM FOR");
+Console.WriteLine("---------------------------------------------------------");
+
+int numero2 = 100;
+for(int index = 1; index <= numero2; index++)
+{
+    if(index % 2 == 1)
+    {
+        Console.WriteLine(index);
     }
 }

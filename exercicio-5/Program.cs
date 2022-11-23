ebd0be8d96c72023d32c8fe8a21a7fc00af572e3
Davi Kennedy Alves Rodrigues <davi_kennedy@live.com>
2022-11-22 22:38:54 -0300

    Adicionando resolução do exercício "Cadastrar" cliente :white_check_mark

---
+++ exercicio-5/Program.cs
@@ -0,0 +1,8 @@
+﻿// "Cadastrar" cliente
+Console.Write("Por favor, digite o seu nome: ");
+string nomeCliente = Console.ReadLine();
+
+Console.Write("Por favor, digite o seu e-mail: ");
+string emailCliente = Console.ReadLine();
+
+Console.WriteLine($"Olá, {nomeCliente}! Usuário {emailCliente} cadastrado!");

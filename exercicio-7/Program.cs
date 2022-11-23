5ed4417ab3e6213a3152ba1f5fdb70bcd13f70b0
Davi Kennedy Alves Rodrigues <davi_kennedy@live.com>
2022-11-22 23:49:14 -0300

    Adicionando resolução do exercício 'Calcular IMC' :white_check_mark

---
+++ exercicio-7/Program.cs
@@ -0,0 +1,19 @@
+﻿// Calcular IMC
+
+string input = "";
+float peso = 0;
+float altura = 0;
+float imc = 0;
+
+Console.Write("Calcular IMC\n\n");
+Console.Write("Insira o seu peso: ");
+input = Console.ReadLine();
+peso = Convert.ToSingle(input);
+
+Console.Write("Insira a sua altura: ");
+input = Console.ReadLine();
+altura = Convert.ToSingle(input);
+
+
+imc = peso / (altura * altura);
+Console.WriteLine($"Seu IMC é: {imc}");
---
+++ exercicio-7/README.md
@@ -0,0 +1,5 @@
+## Descrição
+Crie um programa que solicite o peso e a altura de um usuário, calcule seu Índice de Massa Corporal (IMC) e exiba a situação na saída do console.
+
+## Dica
+* Você pode pesquisar sobre o cálculo do IMC e a sua tabela informativa neste link: https://pt.wikipedia.org/wiki/%C3%8Dndice_de_massa_corporal.

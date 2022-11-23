a290d178bbf951e3526b194a075ea78907c5aacf
Hebert <hebert.moura@hotmail.com>
2022-11-22 22:50:58 -0300

    Conclusão das atividades da semana 2 - Introdução ao .NET e C#

---
+++ Atividade-Wiz-Semana2/5-CadastrarCliente/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+class Programa
+{
+    static void Main(string[] args)
+    {
+        string nome, email;
+
+        Console.WriteLine("Criar um novo cliente: ");
+        Console.Write("Qual é o seu nome?: ");
+        nome = Console.ReadLine();
+        Console.Write("Qual é o seu email?: ");
+        email = Console.ReadLine();
+
+        Console.WriteLine("Olá, " + nome + "! Usuário " + email + " cadastrado!");
+
+        Console.Write("\n");
+        Console.WriteLine("Tecle enter para fechar o programa...");
+        Console.ReadLine();
+    }
+}

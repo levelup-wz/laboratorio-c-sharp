6a7c652560b7305351a90926b4ae2725bc727c82
André luiz azevedo <andreazevedoluiz@gmail.com>
2022-11-25 17:00:36 -0300

    exercicios

---
+++ Semana02/CalculoMedia/Program.cs
@@ -0,0 +1,20 @@
+﻿
+    using System;
+class Programa
+{
+    static void Main(string[] args)
+    {
+        double somaNotas = 6.8 + 7.9 + 6.1 + 10 + 5.4;
+        double media = somaNotas / 5;
+
+        Console.WriteLine("A média da turma foi de: " + media);
+        if (media >= 7)
+        {
+            Console.WriteLine("Turma aprovada!");
+        }
+
+    }
+
+    Console.WriteLine("Tecle enter para fechar");
+        Console.ReadLine();
+}
---
+++ Semana02/contarImpar/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+class Programa
+{
+    static void Main(string[] args)
+    
+    {
+        int i;
+
+        Console.WriteLine("Números Impares de 1 a 100 com o laço FOR");
+        for (i = 0; i <= 100; i++)
+        {
+            
+            if (i % 2 == 1)
+            {
+                Console.WriteLine(i);
+       
+            }
+        }
+        Console.WriteLine("Números Impares de 1 a 100 com o laço WHILE");
+
+        i = 0;
+
+        while (i <= 100)
+        {
+            if (i % 2 == 1)
+            {
+                Console.WriteLine(i);
+
+            }
+         i++;
+        }
+
+
+        Console.WriteLine("Tecle enter para fechar");
+        Console.ReadLine();
+
+
+    }
+}

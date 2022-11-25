2c5524dc6f1a336785c700ca4db4b236df5489c4
Isabela Queiroz <106333972+IsabelaQM@users.noreply.github.com>
2022-11-25 15:29:14 -0300

    Add files via upload

---
+++ Semana 2 - Isabela/Fibonacci.cs
@@ -0,0 +1,20 @@
+﻿using System;
+class Program
+{
+    static void Main(string[] args)
+    {
+        int n1 = 0;
+        int n2 = 1;
+
+        for (int i = 0; i <= 20; i++)
+        {
+            Console.Write(n1 + ", ");
+
+            n2 += n1;
+            n1 = n2 - n1;
+        }
+
+        Console.WriteLine("\n Aperte enter para encerrar ...");
+        Console.ReadLine();
+    }
+}
---
+++ Semana 2 - Isabela/Impares.cs
@@ -0,0 +1,27 @@
+﻿using System;
+class Program
+{
+    static void Main(string[] args)
+    {
+        int i = 0;
+        for (i = 0; i <= 100; i++)
+        {
+            if(i % 2 != 0)
+            {
+                Console.WriteLine(i);
+            }
+        }
+        Console.WriteLine("\n Aperte enter para encerrar.");
+        Console.ReadLine();
+
+        /*int j = 0;
+        while(j<100)
+        {
+            if (j % 2 != 0)
+            {
+                Console.WriteLine(j);
+            }
+            j=j+1;
+        }*/
+    }
+}

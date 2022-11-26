3f5fd7892811dd6d5ba3896af22f518e1e007165
angelicapedroso <angelicapedroso05@gmail.com>
2022-11-26 18:41:43 -0300

    refacrtor: remove comentarios

--- semana-2/SequenciaFibonacci/Program.cs
+++ semana-2/SequenciaFibonacci/Program.cs
@@ -4,8 +4,6 @@ class Programa
 {
   static void Main(string[] args)
   {
-    // Imprimir a sequência de Fibonacci até o vigésimo elemento:
-// 1, 1, 2, 3, 5, 8, 13, 21, 34, 55, 89, 144, 233, 377, 610, 987, 1597, 2584, 4181, 6765.
     // resolvido com 3 variáveis
     int anterior = 0;
     int atual = 1;

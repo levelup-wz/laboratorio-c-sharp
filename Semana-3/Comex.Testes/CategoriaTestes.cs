7fd8d5106c12823c0ef3502a8388814fd7cee37f
Saulo Alves Magalhães <saulo.magalhaes@outlook.com.br>
2022-11-29 18:07:36 -0300

    tests: cria teste para o metodo InformacoesDaCategoria

--- Semana-3/Comex.Testes/CategoriaTestes.cs
+++ Semana-3/Comex.Testes/CategoriaTestes.cs
@@ -8,8 +8,16 @@ namespace Comex.Testes
         public void TestaCriacaoDeCategoria()
         {
             Categoria categoria = new Categoria(1, "INFORM햀ICA", "INATIVA");
+            categoria.Id.Should().Be(1);
             categoria.Nome.Should().Be("INFORM햀ICA");
             categoria.Status.Should().Be("INATIVA");
+        }
+
+        [Fact]
+        public void TestaInformacoesDaCategoria()
+        {
+            Categoria categoria = new Categoria(1, "INFORM햀ICA", "INATIVA");
+            categoria.Informacoes().Should().Be("INFORM햀ICA (1 - INATIVA)");
         }
     }
 }

f74cf5ba5fca807088bcb54ccef6188c4fa6de84
Saulo Alves Magalhães <saulo.magalhaes@outlook.com.br>
2022-11-29 18:08:24 -0300

    tests: cria testes para a classe Produto

---
+++ Semana-3/Comex.Testes/ProdutoTestes.cs
@@ -0,0 +1,28 @@
+﻿using FluentAssertions;
+
+namespace Comex.Testes
+{
+    public class ProdutoTestes
+    {
+        [Fact]
+        public void TestaInformacoesDoProduto()
+        {
+            Produto produto = new Produto(1, "Notebook Samsung", 3523.00, 1, "Informática");
+            produto.InformacoesDoProduto().Should().Be(" Id: 1\n Nome: Notebook Samsung\n Preço Unitário: 3523\n Quantidade em Estoque: 1\n Categoria: Informática\n");
+        }
+        
+        [Fact]
+        public void TestaValorEmEstoque()
+        {
+            Produto produto = new Produto(1, "Notebook Samsung", 3523.00, 1, "Informática");
+            produto.ValorTotalEmEstoque().Should().Be("Valor total do produto em estoque: R$ 3523\n");
+        }
+
+        [Fact]
+        public void TestaCalculaImposto()
+        {
+            Produto produto = new Produto(1, "Notebook Samsung", 3523.00, 1, "Informática");
+            produto.CalculaImposto().Should().Be("Imposto: R$ 1409,2");
+        }
+    }
+}

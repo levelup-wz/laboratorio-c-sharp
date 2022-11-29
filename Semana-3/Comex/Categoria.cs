e117209f5b930662bc03e61911bb65ce2cccc618
Saulo Alves Magalhães <saulo.magalhaes@outlook.com.br>
2022-11-29 09:55:21 -0300

    feat: cria a classe Categoria

---
+++ Semana-3/Comex/Categoria.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Comex
+{
+    public class Categoria
+    {
+        public int Id { get; set; }
+        public string Nome { get; set; }
+        public string Status { get; set; }
+
+        public Categoria(int id, string nome, string status = "ATIVA")
+        {
+            Id = id;
+            Nome = nome;
+            Status = status;
+        }
+
+        public string Informacoes()
+        {
+            return $"{Nome} ({Id} - {Status})";
+        }
+    }
+}

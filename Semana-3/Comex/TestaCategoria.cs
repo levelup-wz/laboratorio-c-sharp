3a8cab69f2721d0b49996076ab0874b9a1eb180a
Saulo Alves Magalhães <saulo.magalhaes@outlook.com.br>
2022-11-29 09:55:37 -0300

    feat: cria a classe TestaCategoria

---
+++ Semana-3/Comex/TestaCategoria.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Comex
+{
+    public class TestaCategoria
+    {
+        Categoria categoria1 = new Categoria(1, "INFORMÁTICA");
+        Categoria categoria2 = new Categoria(2, "MÓVEIS", "INATIVA");
+        Categoria categoria3 = new Categoria(3, "LIVROS");
+
+        public TestaCategoria()
+        {
+            Console.WriteLine(categoria1.Informacoes());
+            Console.WriteLine(categoria2.Informacoes());
+            Console.WriteLine(categoria3.Informacoes());
+        }
+    }
+}

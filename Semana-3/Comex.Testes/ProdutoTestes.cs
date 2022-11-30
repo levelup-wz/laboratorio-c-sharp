69dc44fee8ff4f0dcd4317bf872cbf59d6f72a28
Saulo Alves Magalhães <saulo.magalhaes@outlook.com.br>
2022-11-30 15:46:01 -0300

    test: corrige o retorno dos testes para a classe Produto

--- Semana-3/Comex.Testes/ProdutoTestes.cs
+++ Semana-3/Comex.Testes/ProdutoTestes.cs
@@ -8,21 +8,21 @@ namespace Comex.Testes
         public void TestaInformacoesDoProduto()
         {
             Produto produto = new Produto(1, "Notebook Samsung", 3523.00, 1, "Informática");
-            produto.InformacoesDoProduto().Should().Be(" Id: 1\n Nome: Notebook Samsung\n Preço Unitário: 3523\n Quantidade em Estoque: 1\n Categoria: Informática\n");
+            produto.InformacoesDoProduto().Should().Be(" Id: 1\n Nome: Notebook Samsung\n Preço Unitário: R$ 3.523,00\n Quantidade em Estoque: 1\n Categoria: Informática\n");
         }
         
         [Fact]
         public void TestaValorEmEstoque()
         {
-            Produto produto = new Produto(1, "Notebook Samsung", 3523.00, 1, "Informática");
-            produto.ValorTotalEmEstoque().Should().Be("Valor total do produto em estoque: R$ 3523\n");
+            Produto produto = new Produto(1, "Notebook Samsung", 3523.00, 2, "Informática");
+            produto.ValorTotalEmEstoque().Should().Be(7046.00);
         }
 
         [Fact]
         public void TestaCalculaImposto()
         {
-            Produto produto = new Produto(1, "Notebook Samsung", 3523.00, 1, "Informática");
-            produto.CalculaImposto().Should().Be("Imposto: R$ 1409,2");
+            Produto produto = new Produto(1, "Notebook Samsung", 3523.00, 3, "Informática");
+            produto.CalculaImposto().Should().Be(1409.20);
         }
     }
 }

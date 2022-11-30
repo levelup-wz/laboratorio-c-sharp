c3ed4cf11f288bf709b028ca96e2f6f18aa718cf
Saulo Alves Magalhães <saulo.magalhaes@outlook.com.br>
2022-11-30 19:57:19 -0300

    feat: cria classe Cliente e TestaCliente

---
+++ Semana-3/Comex/Cliente.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Comex
+{
+    public class Cliente
+    {
+        public int Id { get; set; }
+        public string PrimeiroNome { get; set; }
+        public string Sobrenome { get; set; }
+        public string Cpf { get; set; }
+        public string Telefone { get; set; }
+        public string Rua { get; set; }
+        public string NumeroDoEndereco { get; set; }
+        public string Complemento { get; set; }
+        public string Cidade { get; set; }
+        public string Estado { get; set; }
+
+
+        public Cliente(int id, string primeiroNome, string sobrenome, string cpf, string telefone, string rua, string numeroDoEndereco, string complemento, string cidade, string estado )
+        {
+            Id = id;
+            PrimeiroNome= primeiroNome;
+            Sobrenome= sobrenome;
+            Cpf= cpf;
+            Telefone= telefone;
+            Rua= rua;
+            NumeroDoEndereco = numeroDoEndereco;
+            Complemento= complemento;
+            Cidade= cidade;
+            Estado= estado;
+        }
+
+        public string NomeCompleto()
+        {
+            return $"{PrimeiroNome} {Sobrenome}"; 
+        }
+
+        public string EnderecoCompleto()
+        {
+            return $" {Rua}, {NumeroDoEndereco}, {Complemento}\n {Cidade} - {Estado}";
+        }
+    }
+}

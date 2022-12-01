cfd894b1921ea768d3030379cc6bd0e9846bfb08
Saulo Alves Magalhães <saulo.magalhaes@outlook.com.br>
2022-12-01 11:38:08 -0300

    test: altera o arrange nos testes removendo o argumento id

--- Semana-3/Comex.Testes/CategoriaTestes.cs
+++ Semana-3/Comex.Testes/CategoriaTestes.cs
@@ -4,10 +4,11 @@ namespace Comex.Testes
 {
     public class CategoriaTestes
     {
+
         [Fact]
         public void TestaCriacaoDeCategoria()
         {
-            Categoria categoria = new Categoria(1, "INFORM햀ICA", "INATIVA");
+            Categoria categoria = new Categoria("INFORM햀ICA", "INATIVA");
             categoria.Id.Should().Be(1);
             categoria.Nome.Should().Be("INFORM햀ICA");
             categoria.Status.Should().Be("INATIVA");
@@ -16,7 +17,7 @@ namespace Comex.Testes
         [Fact]
         public void TestaInformacoesDaCategoria()
         {
-            Categoria categoria = new Categoria(1, "INFORM햀ICA", "INATIVA");
+            Categoria categoria = new Categoria("INFORM햀ICA", "INATIVA");
             categoria.Informacoes().Should().Be("INFORM햀ICA (1 - INATIVA)");
         }
     }
--- Semana-3/Comex.Testes/ProdutoTestes.cs
+++ Semana-3/Comex.Testes/ProdutoTestes.cs
@@ -7,21 +7,21 @@ namespace Comex.Testes
         [Fact]
         public void TestaInformacoesDoProduto()
         {
-            Produto produto = new Produto(1, "Notebook Samsung", 3523.00, 1, "Informática");
+            Produto produto = new Produto("Notebook Samsung", 3523.00, 1, "Informática");
             produto.InformacoesDoProduto().Should().Be(" Id: 1\n Nome: Notebook Samsung\n Preço Unitário: R$ 3.523,00\n Quantidade em Estoque: 1\n Categoria: Informática\n");
         }
         
         [Fact]
         public void TestaValorEmEstoque()
         {
-            Produto produto = new Produto(1, "Notebook Samsung", 3523.00, 2, "Informática");
+            Produto produto = new Produto("Notebook Samsung", 3523.00, 2, "Informática");
             produto.ValorTotalEmEstoque().Should().Be(7046.00);
         }
 
         [Fact]
         public void TestaCalculaImposto()
         {
-            Produto produto = new Produto(1, "Notebook Samsung", 3523.00, 3, "Informática");
+            Produto produto = new Produto("Notebook Samsung", 3523.00, 3, "Informática");
             produto.CalculaImposto().Should().Be(1409.20);
         }
     }

20322c84f6a6f7928060f777eb8c8ba8d0e2706b
Davi Kennedy Alves Rodrigues <davi_kennedy@live.com>
2022-12-02 12:09:26 -0300

    Criando a classe Categoria

---
+++ Comex/Comex/Categoria.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Comex
+{
+    public class Categoria
+    {
+        public int Id { get; set; }
+        public string Nome { get; set; }
+        public OpcoesStatus Status { get; set; }
+
+        public Categoria(OpcoesStatus status = OpcoesStatus.ATIVA) 
+        {
+            Status = status;
+        }
+    }
+
+    public enum OpcoesStatus
+    {
+        INATIVA = 0,
+        ATIVA = 1
+    }
+}

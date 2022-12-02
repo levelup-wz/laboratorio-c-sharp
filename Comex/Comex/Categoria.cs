53cb440bac4fa29b966cdbb72cd16b82cdad8744
Davi Kennedy Alves Rodrigues <davi_kennedy@live.com>
2022-12-02 15:46:20 -0300

    Refatorando a classe Categoria

--- Comex/Comex/Categoria.cs
+++ Comex/Comex/Categoria.cs
@@ -8,18 +8,17 @@ namespace Comex
 {
     public class Categoria
     {
-        private int _id;
         public int Id { get; set; }
-
         public string Nome { get; set; }
         public OpcoesStatus Status { get; set; }
 
         public Categoria(string nome, OpcoesStatus status = OpcoesStatus.ATIVA) 
         {
-            Nome = nome;
-            Status = status;
-            Id = TotalCategorias + 1;
             TotalCategorias += 1;
+
+            Id = TotalCategorias;
+            Nome = nome;
+            Status = status;    
         }
 
         public static int TotalCategorias { get; set; }

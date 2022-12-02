c4c950955d88b407d0a89b76e40bc7b69ea9ffc5
Davi Kennedy Alves Rodrigues <davi_kennedy@live.com>
2022-12-02 15:46:50 -0300

    Criando a classe Produto

---
+++ Comex/Comex/Produto.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Comex
+{
+    public class Produto
+    {
+        public int Id { get; set; }
+        public string Nome { get; set; }
+        public string Descricao { get; set; }
+        public double PrecoUnitario { get; set; }
+        public int QuantidadeEmEstoque { get; set; }
+        public Categoria ProdutoCategoria { get; set; }
+
+
+        public double CalcularValorTotal()
+        {
+            // Retorna o cálculo do valor total em estoque
+            return PrecoUnitario * QuantidadeEmEstoque;
+        }
+
+        public double CalcularImposto()
+        {
+            // Retorna 40% do preço unitário
+            return PrecoUnitario * 0.4;
+        }
+
+        public Produto(string nome, string descricao, double precoUnitario, int quantidade)
+        {
+            TotalProdutos += 1;
+
+            Id = TotalProdutos;
+            Nome = nome;
+            Descricao = descricao;
+            PrecoUnitario = precoUnitario;
+            QuantidadeEmEstoque += quantidade;
+        }
+
+        public static int TotalProdutos { get; set; }
+    }
+}

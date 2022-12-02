83f5f1f4539f7a669018a7e1f79f13bbe74afa90
Davi Kennedy Alves Rodrigues <davi_kennedy@live.com>
2022-12-02 17:19:52 -0300

    Refatorando as classes Categoria e TestaCategoria

--- Comex/Comex/Program.cs
+++ Comex/Comex/Program.cs
@@ -11,4 +11,20 @@ Console.WriteLine($"{c1.Nome} ({c1.Id} - {c1.Status})");
 Console.WriteLine($"{c2.Nome} ({c2.Id} - {c2.Status})");
 Console.WriteLine($"{c3.Nome} ({c3.Id} - {c3.Status})");*/
 
+Categoria c1 = new Categoria("Informática");
+Categoria c2 = new Categoria("Livros");
+
+Produto p1 = new Produto("Notebook Samsung", 3523.00, 1);
+p1.ProdutoCategoria = c1;
+
+Produto p2 = new Produto("Clean Architecture", 102.90, 2);
+p2.ProdutoCategoria = c2;
+
+Produto p3 = new Produto("Monitor Dell 27", 1889.00, 3);
+p3.ProdutoCategoria = c1;
+
+Console.WriteLine($"Id: {p1.Id}\nNome: {p1.Nome}\nCategoria: {p1.ProdutoCategoria.Nome}\nPreço unitário: R$ {p1.PrecoUnitario}\nQuantidade em estoque: {p1.QuantidadeEmEstoque}\n");
+Console.WriteLine($"Id: {p2.Id}\nNome: {p2.Nome}\nCategoria: {p2.ProdutoCategoria.Nome}\nPreço unitário: R$ {p2.PrecoUnitario}\nQuantidade em estoque: {p2.QuantidadeEmEstoque}\n");
+Console.WriteLine($"Id: {p3.Id}\nNome: {p3.Nome}\nCategoria: {p3.ProdutoCategoria.Nome}\nPreço unitário: R$ {p3.PrecoUnitario}\nQuantidade em estoque: {p3.QuantidadeEmEstoque}\n");
+
 Console.ReadKey();
--- Comex/Comex/Testes/TestaCategoria.cs
+++ Comex/Comex/Testes/TestaCategoria.cs
@@ -1,11 +1,4 @@
-﻿/*using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using Comex;
-
-namespace Comex.Testes
+﻿/*namespace Comex.Testes
 {
     public class TestaCategoria
     {

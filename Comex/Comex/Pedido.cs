8667e1e514704e8ca5bd8b32d5872fbe31521f43
Davi Kennedy Alves Rodrigues <davi_kennedy@live.com>
2022-12-02 19:04:54 -0300

    Criando a classe Pedido

---
+++ Comex/Comex/Pedido.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Comex
+{
+    public class Pedido
+    {
+        public int Id { get; set; }
+        public DateTime Data { get; set; }
+        public string Cliente { get; set; }
+        public Produto PedidoProduto { get; set; }
+        public int QuantidadeVendida { get; set; }
+
+        public double CalcularValorTotal()
+        {
+            return PedidoProduto.PrecoUnitario * QuantidadeVendida;
+        }
+
+        public double CalcularImposto()
+        {
+            return QuantidadeVendida * PedidoProduto.CalcularImposto();
+        }
+
+        public Pedido(string cliente, Produto pedidoProduto, int quantidadeVendida) 
+        {
+            TotalPedidos += 1;
+
+            Id = TotalPedidos;
+            Data = DateTime.Now;
+            Cliente = cliente;
+            PedidoProduto = pedidoProduto;
+            QuantidadeVendida = quantidadeVendida;
+        }
+
+        public static int TotalPedidos { get; set; }
+    }
+}
--- Comex/Comex/Program.cs
+++ Comex/Comex/Program.cs
@@ -9,7 +9,7 @@ Categoria c3 = new Categoria("Livros");
 
 Console.WriteLine($"{c1.Nome} ({c1.Id} - {c1.Status})");
 Console.WriteLine($"{c2.Nome} ({c2.Id} - {c2.Status})");
-Console.WriteLine($"{c3.Nome} ({c3.Id} - {c3.Status})");*/
+Console.WriteLine($"{c3.Nome} ({c3.Id} - {c3.Status})");
 
 Categoria c1 = new Categoria("Informática");
 Categoria c2 = new Categoria("Livros");
@@ -26,5 +26,6 @@ p3.ProdutoCategoria = c1;
 Console.WriteLine($"Id: {p1.Id}\nNome: {p1.Nome}\nCategoria: {p1.ProdutoCategoria.Nome}\nPreço unitário: R$ {p1.PrecoUnitario}\nImposto por unidade: R$ {Math.Round(p1.CalcularImposto(), 2)}\nQuantidade em estoque: {p1.QuantidadeEmEstoque}\n");
 Console.WriteLine($"Id: {p2.Id}\nNome: {p2.Nome}\nCategoria: {p2.ProdutoCategoria.Nome}\nPreço unitário: R$ {p2.PrecoUnitario}\nImposto por unidade: R$ {Math.Round(p2.CalcularImposto(), 2)}\nQuantidade em estoque: {p2.QuantidadeEmEstoque}\n");
 Console.WriteLine($"Id: {p3.Id}\nNome: {p3.Nome}\nCategoria: {p3.ProdutoCategoria.Nome}\nPreço unitário: R$ {p3.PrecoUnitario}\nImposto por unidade: R$ {Math.Round(p3.CalcularImposto(), 2)}\nQuantidade em estoque: {p3.QuantidadeEmEstoque}\n");
+*/
 
 Console.ReadKey();

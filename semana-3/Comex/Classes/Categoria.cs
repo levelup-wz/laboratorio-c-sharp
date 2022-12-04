14b0fc558c88a745f289b63b8875c3651ea1a983
angelicapedroso <angelicapedroso05@gmail.com>
2022-12-03 21:59:51 -0300

    refactor: adiciona valor padrao do status no construtor

--- semana-3/Comex/Classes/Categoria.cs
+++ semana-3/Comex/Classes/Categoria.cs
@@ -4,12 +4,13 @@ namespace Comex
   {
     public static int Id { get; set; }
     public string Nome { get; set; }
-    public string Status { get; set; } = "ATIVA";
+    public string Status { get; set; }
 
-    public Categoria(string nome)
+    public Categoria(string nome, string status = "ATIVA")
     {
       Id = Id + 1;
       Nome = nome;
+      Status = status;
     }
   }
 }

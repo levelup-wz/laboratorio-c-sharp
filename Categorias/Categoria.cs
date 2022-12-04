283aabecb3c719808116747c31eac8b2fa9be17f
Danilo Siervi <71851373+danilosiervi@users.noreply.github.com>
2022-12-04 16:16:38 -0300

    Update Categoria.cs

--- Categorias/Categoria.cs
+++ Categorias/Categoria.cs
@@ -4,15 +4,15 @@ namespace Comex.Categorias
 {
     public class Categoria
     {
-        public int id { get; set; }
-        public string nome { get; set; }
-        public string status { get; set; }
+        public int Id { get; set; }
+        public string Nome { get; set; }
+        public string Status { get; set; }
 
         public Categoria(int id, string nome, string status)
         {
-            this.id = id;
-            this.nome = nome;
-            this.status = status;
+            Id = id;
+            Nome = nome;
+            Status = status;
         }
     }
 }

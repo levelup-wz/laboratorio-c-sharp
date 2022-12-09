fab0649d1ce609ae378c1ddca13486bb6172e4d8
Davi Kennedy Alves Rodrigues <davi_kennedy@live.com>
2022-12-09 17:34:08 -0300

    Adicionando a classe Estoque

---
+++ Comex/Comex/Estoque.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Comex
+{
+    public class Estoque
+    {
+        static int Capacidade { get; private set; } = 1000;
+        static int Ocupacao { get; private set; }
+        static decimal Montante { get; private set; }
+        public Produto Produto { get; set; }
+
+        public void RegistraEntrada(Produto produto) 
+        {
+            Capacidade -= produto.QuantidadeEmEstoque;
+            Ocupacao += produto.QuantidadeEmEstoque;
+            Montante += Convert.ToDecimal(produto.CalcularValorTotal());
+        }
+
+        public void RegistraSaida(Produto produto)
+        {
+            Capacidade += produto.QuantidadeEmEstoque;
+            Ocupacao -= produto.QuantidadeEmEstoque;
+            Montante -= Convert.ToDecimal(produto.CalcularValorTotal());
+        }
+    }
+}

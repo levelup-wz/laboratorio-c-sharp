4ac8c739d49f9a31f0e074d13eff28d0a4ad2e93
André luiz azevedo <andreazevedoluiz@gmail.com>
2022-12-09 22:01:11 -0300

    Criando estoque

---
+++ Semana03/Comex/Comex/Estoque.cs
@@ -0,0 +1,37 @@
+﻿using Comex;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Comex
+{
+    public class Estoque
+    {
+        static public int Capacidade { get; private set; } = 1000;
+        static public int Ocupacao { get; private set; }
+        static public decimal Montante { get; private set; }
+        public Produto Produto { get; set; }
+
+        public void RegistarEntrada (Produto produto)
+        {
+            Capacidade -= produto.Quantidade;
+            Ocupacao += produto.Quantidade;
+            Montante += Convert.ToDecimal(produto.ValorEstoque());
+        }
+
+        public void RegistarSaida(Produto produto)
+        {
+            Capacidade += produto.Quantidade;
+            Ocupacao -= produto.Quantidade;
+            Montante -= Convert.ToDecimal(produto.ValorEstoque());
+        }
+
+    }
+}
+
+
+
+  
+

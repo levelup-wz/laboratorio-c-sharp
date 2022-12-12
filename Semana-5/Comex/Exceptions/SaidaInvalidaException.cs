219dd4aecdbcca5a47f1c5a133198056cac579d0
Saulo Alves Magalhães <saulo.magalhaes@outlook.com.br>
2022-12-12 17:29:36 -0300

    feat: cria exceções para entrada e saida de produtos do estoque

---
+++ Semana-5/Comex/Exceptions/SaidaInvalidaException.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Comex.Exceptions
+{
+    public class SaidaInvalidaException : Exception
+    {
+        public SaidaInvalidaException() { }
+        public SaidaInvalidaException(string mensagem) : base(mensagem) { }
+        public SaidaInvalidaException(string message, Exception inner) : base(message, inner) { }
+        protected SaidaInvalidaException(System.Runtime.Serialization.SerializationInfo info,
+        System.Runtime.Serialization.StreamingContext context) : base(info, context) { }
+    }
+}

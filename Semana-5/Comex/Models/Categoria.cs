d5eac558175c6b132b66d07736c87e13ac210c6a
Saulo Alves Magalhães <saulo.magalhaes@outlook.com.br>
2022-12-12 20:26:36 -0300

    feat: cria método para validar dados ao instanciar uma Categoria

--- Semana-5/Comex/Models/Categoria.cs
+++ Semana-5/Comex/Models/Categoria.cs
@@ -5,7 +5,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace Comex
+namespace Comex.Models
 {
     public class Categoria
     {
@@ -17,13 +17,30 @@ namespace Comex
         public Categoria(string nome, string status = "ATIVA")
         {
             Id = ++_id;
-            Nome = nome;
-            Status = status;
+            ValidaEntradaDeDados(nome, status);
         }
 
         public string Informacoes()
         {
             return $"{Nome} ({Id} - {Status})";
+        }
+
+        private void ValidaEntradaDeDados(string nome, string status)
+        {
+            if(nome.Length < 3)
+            {
+                throw new ArgumentException("O argumento nome deve ser maior que 0.", nameof(nome));
+            }
+
+            if (status.Equals("ATIVA") || status.Equals("INATIVA"))
+            {
+                Nome = nome;
+                Status = status;
+            }
+            else
+            {
+                throw new ArgumentException("O argumento status deve ser ATIVA ou INATIVA", nameof(status));
+            }
         }
     }
 }

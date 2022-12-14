38ee3f99fe6981190ebfe1d98250583fd666ed48
Saulo Alves Magalhães <saulo.magalhaes@outlook.com.br>
2022-12-13 21:47:33 -0300

    feat: cria método para validar dados recebidos no construtor de um Produto

--- Semana-5/Comex/Models/Produto.cs
+++ Semana-5/Comex/Models/Produto.cs
@@ -19,10 +19,7 @@ namespace Comex.Models
         public Produto(string nome, double precoUnitario, int quantidadeEmEstoque, string categoria)
         {
             Id = ++_id;
-            Nome = nome;
-            PrecoUnitario = precoUnitario;
-            QuantidadeEmEstoque = quantidadeEmEstoque;
-            Categoria = categoria;
+            validaEntradaDeDados(nome, precoUnitario, quantidadeEmEstoque, categoria);
         }
 
         public string InformacoesDoProduto()
@@ -39,6 +36,30 @@ namespace Comex.Models
         {
             return PrecoUnitario * 0.40;
         }
-        
+
+        public void validaEntradaDeDados(string nome, double precoUnitario, int quantidadeEmEstoque, string categoria)
+        {
+            if(nome.Length < 5 )
+            {
+                throw new ArgumentException("O nome deve ser maior que 5 caracteres", nameof(nome));
+            }
+            if(precoUnitario <= 0)
+            {
+                throw new ArgumentException("O preço unitário deve ser maior que 0", nameof(precoUnitario));
+            }
+            if(quantidadeEmEstoque <= 0)
+            {
+                throw new ArgumentException("A quantidade em estoque deve ser maior que 0", nameof(quantidadeEmEstoque));
+
+            }
+            if (String.IsNullOrEmpty(categoria))
+            {
+                throw new ArgumentException("A categoria deve ser informada", nameof(categoria));
+            }
+            Nome = nome;
+            PrecoUnitario = precoUnitario;
+            QuantidadeEmEstoque = quantidadeEmEstoque;
+            Categoria = categoria;
+        }
     }
 }

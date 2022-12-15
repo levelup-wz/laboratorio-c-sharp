d4215389a062073c8fc3d0da2f2b788402946dc6
tamaracosta <tamarasantosc@gmail.com>
2022-12-15 16:40:56 +0100

    Atividade Categoria

---
+++ Program.cs
@@ -0,0 +1,24 @@
+﻿//using Comex.Entidades;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Comex
+{
+    class Program
+    {
+        public static void Main(string[] args)
+
+        {
+            TestCategory testeCategoria = new TestCategory();
+            testeCategoria.ToList();
+            
+           
+
+
+        }
+    }
+}
---
+++ TestCategory.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Comex
+{
+    class TestCategory
+    {
+
+        List<Categories> listCategories = new List<Categories>();
+
+        public void ToList()
+        {
+            Categories category1 = new Categories("INFORMÁTICA", "ATIVA");
+            category1.Id = 1;
+            Categories category2 = new Categories("MÓVEIS", "INATIVA");
+            category2.Id = 2;
+            Categories category3 = new Categories("LIVROS", "ATIVA");
+            category3.Id = 3;
+
+            listCategories.Add(category1);
+            listCategories.Add(category2);
+            listCategories.Add(category3);
+
+            Console.WriteLine("1) LISTANDO CATEGORIAS...");
+            foreach (Categories value in listCategories)
+            {
+                Console.WriteLine($"Categoria:  {value.Name} ({value.Id} - {value.Status})");
+                Console.WriteLine("-------------------------------------------------------");
+            }
+
+        }
+
+    }
+}

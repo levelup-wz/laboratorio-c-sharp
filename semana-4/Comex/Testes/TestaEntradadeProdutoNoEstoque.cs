79594d64cac07d1d58d494366539f22f5c6abd5b
angelicapedroso <angelicapedroso05@gmail.com>
2022-12-15 22:21:48 -0300

    altera nome

---
+++ semana-4/Comex/Testes/TestaEntradadeProdutoNoEstoque.cs
@@ -0,0 +1,35 @@
+// namespace Comex.Testes
+// {
+//   public class TestaEntradaDeProdutoNoEstoque
+//   {
+//     public static void Main(string[] args)
+//     {
+//       Estoque estoqueUm = new Estoque();
+//       Categoria categoriaSaude = new Categoria("Saúde");
+//       ProdutoIsento produtoUm = new ProdutoIsento("Vacina", "Vacina ...", 250.00, 500, categoriaSaude);
+//       estoqueUm.RegistraEntrada(produtoUm);
+//       Console.WriteLine($"Capacidade: {estoqueUm.Capacidade}\n" +
+//       $"Ocupação: {estoqueUm.Ocupacao}\n" +
+//       $"Montante: {estoqueUm.Montante}\n"
+//       );
+      
+//       Estoque estoqueDois = new Estoque();
+//       Categoria categoriaLivros = new Categoria("Livros");
+//       ProdutoIsento produtoDois = new ProdutoIsento("Use a Cabeça: Java", "Programação", 112.90, 5, categoriaLivros);
+//       estoqueDois.RegistraEntrada(produtoDois);
+//       Console.WriteLine($"Capacidade: {estoqueDois.Capacidade}\n" +
+//       $"Ocupação: {estoqueDois.Ocupacao}\n" +
+//       $"Montante: {estoqueDois.Montante}\n"
+//       );
+
+//       Estoque estoqueTres = new Estoque();
+//       Categoria categoriaInformatica = new Categoria("Informática");
+//       Produto produtoTres = new Produto("Macbook Pro", "Última geração", 20000.00, 15, categoriaInformatica);
+//       estoqueTres.RegistraEntrada(produtoTres);
+//       Console.WriteLine($"Capacidade: {estoqueTres.Capacidade}\n" +
+//       $"Ocupação: {estoqueTres.Ocupacao}\n" +
+//       $"Montante: {estoqueTres.Montante}\n"
+//       );
+//     }
+//   }
+// }
---
+++ semana-4/README.md
@@ -0,0 +1,24 @@
+# Comex
+
+O projeto **Comex** é um sistema de _e-commerce marketplace_. Seu objetivo é permitir que a sua base de clientes tenha acesso a vários vendedores e, assim, consolidar-se como o shopping eletrônico mais popular da internet.
+
+## Requisitos
+✅ Criar uma classe `Categoria` para representar a categoria dos produtos do comércio eletrônico.
+
+✅ Verificar se a classe `Categoria` funciona adequadamente.
+
+✅ Criar uma classe `Produto` para representar os produtos vendidos no comércio eletrônico.
+
+✅ Verificar se a classe `Produto` funciona adequadamente.
+
+✅ Criar uma classe `Pedido` para registrar os pedidos dos clientes. Neste momento, o comércio eletrônico só permite vender um produto por pedido.
+
+✅ Verificar se a classe `Pedido` funciona adequadamente.
+
+✅ Criar uma classe `Cliente` para representar o cliente das vendas e as informações para identificá-lo e enviar o pedido.
+
+✅ Alterar a classe `Pedido` para ter um cliente completo, não somente seu CPF;
+
+✅ Alterar o arquivo `TestaPedido` para utilizar a classe `Cliente` e imprimir o nome completo do cliente e seu endereço.
+
+✅ Fazer com que, ao criar um objeto, o `id` seja definido automaticamente, sempre incrementando o valor anterior.

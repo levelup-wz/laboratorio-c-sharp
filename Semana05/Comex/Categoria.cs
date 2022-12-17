82eb7da82922b764497dd07689c387f315810088
André luiz azevedo <andreazevedoluiz@gmail.com>
2022-12-16 21:47:04 -0300

    Validação de categoria

--- Semana05/Comex/Categoria.cs
+++ Semana05/Comex/Categoria.cs
@@ -3,6 +3,7 @@ using System;
 using System.Collections.Generic;
 using System.Globalization;
 using System.Linq;
+using System.Net.NetworkInformation;
 using System.Security.Cryptography;
 using System.Text;
 using System.Threading.Tasks;
@@ -20,11 +21,36 @@ namespace Comex
         public Categoria CategoriaClasse { get; set; }
 
 
-        public Categoria(string nome, String status) {
+        public Categoria(string nome, String status = "Ativa") {
             Id = ++ContId;
             Nome = nome;
             Status = status;
         }
+
+        public void  ValidacaoDeCategoria(string nome, String status)
+        {
+            if(Id <= 0)
+            {
+                throw new ArgumentException("O ID deve ser maior do que zero");
+            }
+            if(nome.Length <= 3) 
+            {
+                throw new ArgumentException("O nome deve ter mais que três letras");
+            }
+            if(status != "Ativa" || status != "Inativa")
+            {
+                throw new ArgumentException("Status deve ser Ativa ou Inativa");
+            }
+            else
+            {
+                Id = ++ContId;
+                Nome = nome;
+                Status = status;
+                Console.WriteLine($"A categoria {Nome} foi criada com sucesso!!!");
+            }
+
+        }
+      
 
         public string RetornaInfos()
         {

4bde804657fd856e92b1fe55eb9d4634c334a7ac
Isabela Queiroz <106333972+IsabelaQM@users.noreply.github.com>
2022-12-19 11:07:20 -0300

    Upload dos arquivos

---
+++ Program.cs
@@ -0,0 +1,9 @@
+﻿using Comex;
+Console.WriteLine("Testando a classe de Produto Isento: \n");
+TestaProdutoIsento.Main();
+
+Console.WriteLine("Testando a Entrada de produtos no estoque: \n");
+TestaEntradaDeProdutosNoEstoque.Main();
+
+Console.WriteLine("Testando a Saída de produtos no estoque: \n");
+TestaSaidaDeProdutoNoEstoque.Main();

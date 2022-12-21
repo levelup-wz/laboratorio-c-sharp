62d6687da8aa4d9d745b67785055f65d1d804d74
Saulo Alves Magalhães <saulo.magalhaes@outlook.com.br>
2022-12-20 21:01:34 -0300

    feat: cria a classe NotaFiscal

---
+++ Semana-6/Comex.Models/NotaFiscal.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Comex.Models
+{
+    public class NotaFiscal
+    {
+        internal Pedido Pedido { get; set; }
+        public NotaFiscal(Pedido pedido)
+        {
+            Pedido = pedido;
+        }
+
+        public override string ToString()
+        {
+            return
+                $"Nome: {Pedido.Cliente.NomeCompleto()}\n" +
+                $"Data da compra: {Pedido.Data}\n" +
+                $"Total do pedido: R$ {Pedido.ValorTotal():C}";
+        }
+    }
+}

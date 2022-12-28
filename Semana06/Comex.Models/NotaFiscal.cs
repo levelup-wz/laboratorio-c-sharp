7c0a3dc64409dadf55f5d567de26e93cd762fbf0
André luiz azevedo <andreazevedoluiz@gmail.com>
2022-12-27 21:18:20 -0300

    Criando Nota Fiscal e usando biblioteca NumericWordsConversion

---
+++ Semana06/Comex.Models/NotaFiscal.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NumericWordsConversion;
+
+namespace Comex.Models
+{
+    public class NotaFiscal
+    {
+        internal Pedido Pedido { get; set;}
+        public string ValorExtensoIngles { get; set; }
+
+
+        public NotaFiscal(Pedido pedido)
+        {
+            Pedido = pedido;
+            NumericWordsConverter converter = new NumericWordsConverter();
+            ValorExtensoIngles = converter.ToWords((decimal)pedido.ValorPedido());
+
+        }
+
+        public override string ToString()
+        {
+            return $" Nome completo:{Pedido.ClientePedido.Nome} {Pedido.ClientePedido.Sobrenome} \n" +
+                $"Data da Compra: {Pedido.Data} \n" +
+                $"Total do Pedido: {Pedido.ValorPedido()} \n" +
+                $"Valor Total do Pedido por Extenso em Inglês: {ValorExtensoIngles}";
+        }
+    }
+}
--- Semana06/Comex/Program.cs
+++ Semana06/Comex/Program.cs
@@ -6,7 +6,7 @@ using Comex.Models;
 
 
 
-Console.WriteLine("Teste Categoria:");
+/*Console.WriteLine("Teste Categoria:");
 Console.WriteLine("----------------------------------");
 //new CategoriaTeste();
 CategoriaTeste.Main();
@@ -43,6 +43,12 @@ Console.WriteLine("----------------------------------");
 Console.WriteLine("Teste Validação do Produto:");
 Console.WriteLine("----------------------------------");
 TestaValidacaoDeProdutos.Main();
+Console.WriteLine("----------------------------------");*/
+Console.WriteLine("Teste Nota Fiscal:");
+Console.WriteLine("----------------------------------");
+
+var testeNotaFiscal = new NotaFiscal(new Pedido(new Cliente("André", "Azevêdo", "012.987.852-87", "(64) 98521-8574", "Rua João 123", "14", "Casa 02", "Centro", "Goaindira", "GO"), new Produto("Celular", 4500.00, 1, "Informática"), 5));
+Console.WriteLine(testeNotaFiscal.ToString());
 
 
 

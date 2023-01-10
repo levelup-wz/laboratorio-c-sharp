0255b6de8bbf74000f03b861d1e8393ec5e0a2f1
Saulo Alves Magalhães <saulo.magalhaes@outlook.com.br>
2023-01-10 11:34:09 -0300

    feat: cria classe TesteCPF

---
+++ Semana-7/Comex/TestaModels/TesteCPF.cs
@@ -0,0 +1,16 @@
+﻿using Comex.Models;
+
+namespace Comex.TestaModels
+{
+    public class TesteCPF
+    {
+        static Cpf CPF = new("12345678910");
+
+        public static void Main()
+        {
+            var cpfFormatado = CPF.Formatar();
+            Console.WriteLine($"CPF Formatado: {cpfFormatado}");
+            Console.WriteLine($"CPF Sem Formatação: {CPF}");
+        }
+    }
+}

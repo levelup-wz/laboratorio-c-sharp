2c70c2f90b8ad94ef8cd6503a4dd78ded716d0a2
Saulo Alves Magalhães <saulo.magalhaes@outlook.com.br>
2023-01-10 11:33:51 -0300

    feat: cria a classe CPF com o método formatar

---
+++ Semana-7/Comex.Models/Cpf.cs
@@ -0,0 +1,41 @@
+﻿using System.Text.RegularExpressions;
+
+namespace Comex.Models
+{
+    public class Cpf
+    {
+        public string CpfSemFormatacao { get; set; }
+
+        public Cpf(string cpf)
+        {
+            CpfSemFormatacao = cpf;
+        }
+
+        public string Formatar() 
+        {
+            bool cpfIsNumeric = Regex.IsMatch(CpfSemFormatacao, @"^\d+$");
+
+            if (string.IsNullOrWhiteSpace(CpfSemFormatacao))
+            {
+                return "Cpf deve ser informado";
+            }
+            if (!cpfIsNumeric)
+            {
+                return "Cpf inválido";
+            }
+            if (CpfSemFormatacao.Length != 11)
+            {
+                return "Cpf inválido";
+            }
+
+            return Convert.ToUInt64(CpfSemFormatacao).ToString(@"000\.000\.000\-00");
+
+        }
+
+        public override string ToString()
+        {
+            return CpfSemFormatacao;
+        }
+
+    }
+}

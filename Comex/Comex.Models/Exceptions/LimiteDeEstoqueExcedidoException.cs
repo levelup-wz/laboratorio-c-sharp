9f713d57716aafb0832d21edc557d17948299c86
Danilo Siervi <71851373+danilosiervi@users.noreply.github.com>
2023-01-10 16:17:13 -0300

    Update LimiteDeEstoqueExcedidoException.cs

--- Comex/Comex.Models/Exceptions/LimiteDeEstoqueExcedidoException.cs
+++ Comex/Comex.Models/Exceptions/LimiteDeEstoqueExcedidoException.cs
@@ -1,11 +1,22 @@
 using System;
 
-namespace Comex.Exceptions
+namespace Comex.Modelos.Exceptions
 {
-    public class LimiteDeEstoqueExcedidoException : Exception
+    public class LimiteDeEstoqueExcedidoException
+        : Exception
     {
-        public LimiteDeEstoqueExcedidoException() { }
-        public LimiteDeEstoqueExcedidoException(string mensagem) : base(mensagem) { }
-        public LimiteDeEstoqueExcedidoException(string mensagem, Exception innerException) : base(mensagem, innerException) { }
+        public LimiteDeEstoqueExcedidoException()
+        {
+        }
+        
+        public LimiteDeEstoqueExcedidoException(string mensagem)
+        : base(mensagem)
+        {
+        }
+        
+        public LimiteDeEstoqueExcedidoException(string mensagem, Exception innerException) 
+        : base(mensagem, innerException)
+        {
+        }
     }
 }

602eaa5202ffcabf4a360cc120d42a538742be94
Danilo Siervi <danilosiervidepaula16@gmail.com>
2023-01-19 16:37:48 -0300

    Criar itensdopedido

---
+++ Comex/Comex.Modelos/Pedidos/ItensDoPedido.cs
@@ -0,0 +1,19 @@
+﻿using Comex.Modelos.Produtos;
+using System;
+
+namespace Comex.Modelos.Pedidos
+{
+    public class ItensDoPedido
+    {
+        public Produto Item { get; }
+        public int Quantidade { get; set; }
+        public decimal Total { get; }
+
+        public ItensDoPedido(Produto item, int quantidade)
+        {
+            Item = item;
+            Quantidade = quantidade;
+            Total = (decimal)item.Preco * quantidade;
+        }
+    }
+}

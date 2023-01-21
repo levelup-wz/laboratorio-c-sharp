67e6f7ae4972d27e4094e6ce37a06490a13d60d9
Davi Kennedy Alves Rodrigues <davi_kennedy@live.com>
2023-01-21 12:12:34 -0300

    Criando classe ItemDoPedido

---
+++ Comex/Comex.Models/ItemDoPedido.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Comex.Models
+{
+    public class ItemDoPedido
+    {
+        public Produto Produto { get; private set; }
+        public int Quantidade { get; set; }
+        public decimal Total => (decimal)Produto.PrecoUnitario * Quantidade;
+    }
+}

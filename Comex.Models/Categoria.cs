0cf52f40e9e4d3214974c27b5c17e56c358f1066
Isabela Queiroz <106333972+IsabelaQM@users.noreply.github.com>
2023-01-23 17:26:06 -0300

    Add files via upload

---
+++ Comex.Models/Categoria.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace Comex.Modelos.Categorias
+{
+    public class Categoria
+    {
+        public static int Quantidade { get; private set; }
+        public int Id { get; private set; }
+        public string Nome { get; set; }
+        public string Status { get; set; }
+
+        public Categoria(string nome, string status)
+        {
+            Quantidade++;
+            Id = Quantidade;
+
+            if (nome.Length <= 3)
+            {
+                throw new ArgumentException("Nome deve conter mais do que 3 caracteres");
+            }
+            if (status != "ATIVA" && status != "INATIVA")
+            {
+                throw new ArgumentException("O status deve ser: ATIVA ou INATIVA");
+            }
+
+            Nome = nome;
+            Status = status;
+        }
+    }
+}
---
+++ Comex.Models/NotaFiscal.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NumericWordsConversion;
+
+namespace Comex.Models
+{
+    public class NotaFiscal
+    {
+        internal Pedido Pedido { get; set; }
+        public string PorExtensoIngles { get; set; }
+        public NotaFiscal(Pedido pedido)
+        {
+            Pedido = pedido;
+            NumericWordsConverter converter = new NumericWordsConverter();
+            PorExtensoIngles = converter.ToWords((decimal)Pedido.CalculaValorTotal());
+        }
+
+        public override string ToString()
+        {
+            return
+                $"Nome: {Pedido.Cliente.NomeCompleto()}\n" +
+                $"Data da compra: {Pedido.Data}\n" +
+                $"Total do pedido: {Pedido.CalculaValorTotal():C}\n" +
+                $"Valor por extenso em Inglês: {PorExtensoIngles}\n";
+        }
+    }
+}

4cf0290d90240d41e93a8d803a9767892ca8af6a
Hebert <hebert.moura@hotmail.com>
2023-01-24 17:51:33 -0300

    criação do método EncontrarPeloNome

--- Atividade-Wiz-Semana9/Comex.Models/Extensoes/ProdutosExtensao.cs
+++ Atividade-Wiz-Semana9/Comex.Models/Extensoes/ProdutosExtensao.cs
@@ -1,13 +1,19 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace Comex.Models.Extensoes
 {
-    public class ProdutosExtensao
+    public static class ProdutosExtensao
     {
+        public static Produto EncontrarPeloNome(this List<Produto> lista, string nome)
+        {
+            var produto = lista.FirstOrDefault(x => x.Nome.ToUpper() == nome.ToUpper());
 
+            return produto;
+        }
     }
 }

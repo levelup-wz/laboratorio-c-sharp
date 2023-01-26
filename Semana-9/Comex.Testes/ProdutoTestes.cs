1a3dcdcf5a16c47a6df24a2b2977fecca3754ac9
Saulo Alves Magalhães <saulo.magalhaes@outlook.com.br>
2023-01-26 11:38:26 -0300

    feat: cria método encontrarpelonome para list

--- Semana-9/Comex.Testes/ProdutoTestes.cs
+++ Semana-9/Comex.Testes/ProdutoTestes.cs
@@ -1,4 +1,5 @@
 ﻿using Comex.Models;
+using Comex.Models.Extensoes;
 using FluentAssertions;
 
 namespace Comex.Testes
@@ -24,6 +25,20 @@ namespace Comex.Testes
         {
             Produto produto = new Produto("Notebook Samsung", 3523.00, 3, "Informática");
             produto.CalculaImposto().Should().Be(1409.20);
+        }
+
+        [Fact]
+        public void Deve_encontrar_um_produto_na_lista_pela_descricao()
+        {
+            List<Produto> produtos = new();
+            var camisa = new Produto("Camisa", 10, 10, categoria: "Roupas");
+            var tenis = new Produto("Tenis", 20, 20, categoria: "Roupas");
+            produtos.AddRange(new[] { camisa, tenis });
+
+            Assert.True(produtos.EncontrarPeloNome("Camisa").Nome.Equals("Camisa"));
+            Assert.True(produtos.EncontrarPeloNome("camisa").Nome.Equals("Camisa"));
+            Assert.True(produtos.EncontrarPeloNome("Tenis").Nome.Equals("Tenis"));
+            Assert.True(produtos.EncontrarPeloNome("tenis").Nome.Equals("Tenis"));
         }
     }
 }

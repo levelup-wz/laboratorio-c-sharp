13db498160dd26b0a3b52d7624b5f6b76786d20b
Saulo Alves Magalhães <saulo.magalhaes@outlook.com.br>
2023-01-26 11:05:55 -0300

    feat: cria métodos de extensao para extrair tamanho e cor

--- README.md
+++ README.md
@@ -1,5 +1,5 @@
-# Semana 8 - List, lambda, linq
-- [ ] Crie novos métodos de extensão
+# Semana 9 - List, lambda, linq
+- [x] Crie novos métodos de extensão
 - [ ] Crie uma nova extensão para List
 - [ ] Calcular o ValorTotal usando lambda
 - [ ] Calcular TotalDeImpostos usando lambda
--- Semana-9/Comex.Models/Vestuario.cs
+++ Semana-9/Comex.Models/Vestuario.cs
@@ -1,4 +1,6 @@
-﻿namespace Comex.Models
+﻿using Comex.Models.Extensoes;
+
+namespace Comex.Models
 {
     public class Vestuario : Produto
     {
@@ -6,8 +8,8 @@
         public readonly string Cor;
         public Vestuario(string nome, double precoUnitario, int quantidadeEmEstoque, string categoria, string atributos) : base(nome, precoUnitario, quantidadeEmEstoque, categoria, atributos)
         {
-            Tamanho = atributos.Substring(9, 1);
-            Cor = atributos.Substring(17);
+            Tamanho = atributos.ExtrairTamanho();
+            Cor = atributos.ExtrairCor();
         }
     }
 }

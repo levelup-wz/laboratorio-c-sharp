09b9c567b45d6251ac5600ce634173be69a62f09
Davi Kennedy Alves Rodrigues <davi_kennedy@live.com>
2023-01-28 08:50:44 -0300

    Criando método de extensão para encontrar Produto pelo nome

---
+++ Comex/Comex.Models/Extensoes/ProdutoExtensao.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Comex.Models;
+
+namespace Comex.Models.Extensoes
+{
+    public static class ProdutoExtensao
+    {
+        public static Produto EncontrarPeloNome(this List<Produto> produtos, string nome)
+        {
+            foreach (var produto in produtos)
+            {
+                bool nomeEncontrado = produto.Nome.Contains(nome, StringComparison.OrdinalIgnoreCase);
+                if (nomeEncontrado) return produto;
+            }
+
+            return default;
+        }
+    }
+}

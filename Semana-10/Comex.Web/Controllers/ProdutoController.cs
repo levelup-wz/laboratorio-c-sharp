b7c65303aa21d3ca27cc13f206393269cda00396
Saulo Alves Magalhães <saulo.magalhaes@outlook.com.br>
2023-01-30 15:19:58 -0300

    feat: cria rota para adicionar um produto

--- Semana-10/Comex.Web/Controllers/ProdutoController.cs
+++ Semana-10/Comex.Web/Controllers/ProdutoController.cs
@@ -1,15 +1,34 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using AutoMapper;
+using Comex.Models;
+using Comex.Web.Data.Dto;
+using Microsoft.AspNetCore.Mvc;
 
-namespace Comex.Web.Controllers
+namespace Comex.Web.Controllers;
+
+[Route("api/[controller]")]
+[ApiController]
+public class ProdutoController : ControllerBase
 {
-    [Route("api/[controller]")]
-    [ApiController]
-    public class ProdutoController : ControllerBase
+    private static List<Produto> _produtos = new();
+    private readonly IMapper _mapper;
+
+    public ProdutoController(IMapper mapper)
     {
-        [HttpGet]
-        public IActionResult HoraAtual()
-        {
-            return Ok(DateTime.Now);
-        }
+        _mapper = mapper;
     }
+
+    [HttpGet]
+    public IActionResult HoraAtual()
+    {
+        return Ok(DateTime.Now);
+    }
+
+    [HttpPost]
+    public IActionResult CriaProduto([FromBody] CriarProdutoDto produtoDto)
+    {
+        var produto = _mapper.Map<Produto>(produtoDto);
+        _produtos.Add(produto);
+        return Created("",produto);
+    }
+
 }
---
+++ Semana-10/Comex.Web/Profiles/ProdutoProfile.cs
@@ -0,0 +1,13 @@
+﻿using AutoMapper;
+using Comex.Models;
+using Comex.Web.Data.Dto;
+
+namespace Comex.Web.Profiles;
+
+public class ProdutoProfile : Profile
+{
+    public ProdutoProfile()
+    {
+        CreateMap<CriarProdutoDto, Produto>().ReverseMap();
+    }
+}

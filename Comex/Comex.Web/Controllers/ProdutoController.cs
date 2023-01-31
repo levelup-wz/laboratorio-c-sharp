d5d436f513d87422b097040785c5a101971d6a94
Danilo Siervi <danilosiervidepaula16@gmail.com>
2023-01-30 23:56:06 -0300

    Adding Comex.Web

---
+++ Comex/Comex.Web/Controllers/ProdutoController.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Comex.Web.Controllers;
+
+[Controller]
+[Route("[controller]")]
+public class ProdutoController : Controller
+{
+
+    public IActionResult Index()
+    {
+        return View();
+    }
+}

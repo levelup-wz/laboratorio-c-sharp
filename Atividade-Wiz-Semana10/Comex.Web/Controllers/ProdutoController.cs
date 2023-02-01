c65a63a54b4007ee50665fed801b1c742b2fa73b
Hebert <hebert.moura@hotmail.com>
2023-02-01 15:53:03 -0300

    Adicionar mètodo para criar um novo Produto

--- Atividade-Wiz-Semana10/Comex.Web/Controllers/ProdutoController.cs
+++ Atividade-Wiz-Semana10/Comex.Web/Controllers/ProdutoController.cs
@@ -1,4 +1,7 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using AutoMapper;
+using Comex.Models;
+using Comex.Web.Data.Dto;
+using Microsoft.AspNetCore.Mvc;
 
 namespace Comex.Web.Controllers
 {
@@ -6,11 +9,27 @@ namespace Comex.Web.Controllers
     [Route("[controller]")]
     public class ProdutoController : ControllerBase
     {
+        private static List<Produto> _produtoList = new();
+        private IMapper _mapper;
+
+        public ProdutoController(IMapper mapper)
+        {
+            _mapper = mapper;
+        }
+
         [HttpGet]
         public IActionResult GetHoraAtual()
         {
             var HoraAtual = DateTime.Now;
             return Ok(HoraAtual);
+        }
+
+        [HttpPost]
+        public IActionResult PostProduto([FromBody] CriarProdutoDTO produtoDto)
+        {
+            var produto = _mapper.Map<Produto>(produtoDto);
+            _produtoList.Add(produto);
+            return Ok();
         }
 
     }

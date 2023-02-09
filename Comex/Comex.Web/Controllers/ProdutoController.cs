166aafe7e0c3ad5b2a62b77d6be76a4d5a0c3197
Davi Kennedy Alves Rodrigues <davi_kennedy@live.com>
2023-02-08 22:41:56 -0300

    Criando um novo Controller para Produto

---
+++ Comex/Comex.Web/Controllers/ProdutoController.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Comex.Web.Controllers;
+
+[ApiController]
+[Route("[controller]")]
+public class ProdutoController : ControllerBase
+{
+    [HttpGet]
+    public DateTime InformarHorario()
+    {
+        return DateTime.Now;
+    }
+}

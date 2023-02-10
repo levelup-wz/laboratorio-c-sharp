17ab010aa03c58792f84a7e2dd3a7b0da3908ee2
André luiz azevedo <andreazevedoluiz@gmail.com>
2023-02-10 15:16:32 -0300

    Criando APi com controller que retorna a data atual

---
+++ laboratorio-c-sharp-semana09/Semana09/Comex.Web/Controllers/ProdutoController.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.VisualBasic;
+
+namespace Comex.Web.Controllers
+{
+
+    [ApiController]
+    [Route("[Controller]")]
+    public class ProdutoController : ControllerBase
+    {
+        [HttpGet]
+        public ActionResult HoraAtual() 
+        {
+            return Ok(DateAndTime.Now);
+
+        }
+
+    }
+}

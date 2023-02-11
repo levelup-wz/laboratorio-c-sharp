bde7e08f67cb0b6b3b8fb7978f92b68a7f0fa26e
André luiz azevedo <andreazevedoluiz@gmail.com>
2023-02-10 21:06:26 -0300

    Obter um produto pelo Id

--- laboratorio-c-sharp-semana09/Semana09/Comex.Models/Produto.cs
+++ laboratorio-c-sharp-semana09/Semana09/Comex.Models/Produto.cs
@@ -1,6 +1,7 @@
 ﻿
 using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -10,6 +11,7 @@ namespace Comex
 {
     public class Produto
     {
+        
         private static int _id = 0;
         public int Id { get; set; }
         public string Nome { get; set; }
--- laboratorio-c-sharp-semana09/Semana09/Comex.Web/Controllers/ProdutoController.cs
+++ laboratorio-c-sharp-semana09/Semana09/Comex.Web/Controllers/ProdutoController.cs
@@ -1,4 +1,5 @@
 ﻿using AutoMapper;
+using Comex.Web.Data.Dto;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.VisualBasic;
 
@@ -14,6 +15,36 @@ namespace Comex.Web.Controllers
     {
         private static List<Produto> _produto = new();
         private IMapper _imapper;
+        private static int id = 1;
+
+        public ProdutoController(IMapper mapper)
+        {
+            _imapper = mapper;
+        }
+
+        [HttpPost]
+        public IActionResult AdicionarProduto ([FromBody] CriarProdutoDTO produtoDto) 
+        {
+            Produto produto = _imapper.Map<Produto>(produtoDto);
+            produto.Id = id++;
+            _produto.Add(produto);
+
+            return CreatedAtAction(nameof(ObterProdutoPorId), new { Id = produto.Id }, produto);
+        }
+
+        [HttpGet("{id}")]
+        public IActionResult ObterProdutoPorId(int id) 
+        {
+            var produto = _produto.FirstOrDefault(produto => produto.Id == id);
+            
+            if(produto != null)
+            {
+                LerProdutoDto produtoDto = _imapper.Map<LerProdutoDto>(produto);
+                return Ok(produtoDto);
+            }
+
+            return NotFound();
+        }
 
 
         [HttpGet]
